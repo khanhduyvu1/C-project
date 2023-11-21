a82669d8908ae7d01bbbe6cf5a9948e153591742
Khanh Vu <khanhduyvu@usf.edu>
2023-11-20 23:05:32 -0500

    ,

--- project7_shelter.c
+++ project7_shelter.c
@@ -3,7 +3,7 @@
  */
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 // Store animals information
 struct Animal {
     char name[100];
@@ -12,7 +12,17 @@ struct Animal {
     int age;
     double weight;
 };
+int compareAnimals(const void *a, const void *b) {
+    // Compare based on species first
+    int speciesComparison = strcmp(((struct Animal *)a)->species, ((struct Animal *)b)->species);
+    if (speciesComparison != 0) {
+        return speciesComparison;
+    }
 
+    // If species is the same, compare based on age
+    int ageComparison = ((struct Animal *)a)->age - ((struct Animal *)b)->age;
+    return ageComparison;
+}
 int main() {
     FILE *inputFile = fopen("animals.txt", "r"); // open animals.txt file
     if (inputFile == NULL) {
@@ -26,6 +36,7 @@ int main() {
         numAnimals++;
     }
     fclose(inputFile);
+    qsort(animals, numAnimals, sizeof(struct Animal), compareAnimals);
     char searchSpecies[100];
     char searchGender[100];
     printf("Enter species: ");
@@ -39,6 +50,7 @@ int main() {
         return 1;
     }
     // print output
+
     for (int i = 0; i < numAnimals; i++) {
         if (strcmp(animals[i].species, searchSpecies) == 0 && strcmp(animals[i].gender, searchGender) == 0) {
             fprintf(outputFile, "%s %d %.2lf\n", animals[i].name, animals[i].age, animals[i].weight);
--- project9_guests.c
+++ project9_guests.c
@@ -1,4 +1,6 @@
 /*
+ * Khanh Vu
+ * U56604117
  * Program manages and tracks the guest list for a local restaurant
  */
 #include <stdio.h>
@@ -9,16 +11,17 @@
 #define PHONE_LEN 20
 
 struct guest{
-	char phone[PHONE_LEN+1];
-	char last[NAME_LEN+1];
-	char first[NAME_LEN+1];
-	int party_size;
-	struct guest *next;
+    char phone[PHONE_LEN+1];
+    char last[NAME_LEN+1];
+    char first[NAME_LEN+1];
+    int party_size;
+    struct guest *next;
 };
 struct guest *add_guest(struct guest *list);
 void print_list(struct guest *list);
 void clear_list(struct guest *list);
 int read_line(char str[], int n);
+struct guest *remove_guest(struct guest *list);
 
 /**********************************************************
  * main: Prompts the user to enter an operation code,     *
@@ -29,26 +32,28 @@ int read_line(char str[], int n);
  **********************************************************/
 int main(void)
 {
-  char code;
+    char code;
 
-  struct guest *new_list = NULL;  
-  printf("Operation Code: a for adding to the list at the end, p for printing the list; q for quit.\n");
-  for (;;) {
-    printf("Enter operation code: ");
-    scanf(" %c", &code);
-    while (getchar() != '\n')   /* skips to end of line */
-      ;
-    switch (code) {
-      case 'a': new_list = add_guest(new_list);
+    struct guest *new_list = NULL;
+    printf("Operation Code: a for adding to the list at the end, r for removing from the list, p for printing the list; q for quit.\n");
+    for (;;) {
+        printf("Enter operation code: ");
+        scanf(" %c", &code);
+        while (getchar() != '\n')   /* skips to end of line */
+            ;
+        switch (code) {
+            case 'a': new_list = add_guest(new_list);
                 break;
-      case 'p': print_list(new_list);
+            case 'p': print_list(new_list);
                 break;
-      case 'q': clear_list(new_list);
-		return 0;
-      default:  printf("Illegal code\n");
+            case 'r': new_list = remove_guest(new_list);
+                break;
+            case 'q': clear_list(new_list);
+                return 0;
+            default:  printf("Illegal code\n");
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
 }
 struct guest *add_guest(struct guest *list){
     char phone[PHONE_LEN+1];
@@ -58,7 +63,7 @@ struct guest *add_guest(struct guest *list){
     struct guest *current = list;
     struct guest *new = (struct guest *)malloc(sizeof(struct guest));
 
-    //enter phone number and check for if it exist or not
+    //enter phone number and check for if it exists or not
     printf("Enter phone number: ");
     scanf("%s", phone);
     while (current != NULL){
@@ -95,6 +100,41 @@ struct guest *add_guest(struct guest *list){
         return list;
     }
 }
+// remove guest
+struct guest *remove_guest(struct guest *list) {
+    char phone[PHONE_LEN + 1];
+    char last[NAME_LEN + 1];
+    char first[NAME_LEN + 1];
+
+    printf("Enter phone number: ");
+    scanf("%s", phone);
+
+    printf("Enter guest's last name: ");
+    read_line(last, NAME_LEN + 1);
+
+    printf("Enter guest's first name: ");
+    read_line(first, NAME_LEN + 1);
+
+    struct guest *current = list;
+    struct guest *prev = NULL;
+
+    while (current != NULL) {
+        if (strcmp(current->phone, phone) == 0 && strcmp(current->last, last) == 0 &&
+            strcmp(current->first, first) == 0) {
+            if (prev == NULL) {
+                list = current->next;
+            } else {
+                prev->next = current->next;
+            }
+            free(current);
+            return list;
+        }
+        prev = current;
+        current = current->next;
+    }
+    printf("Guest does not exist.\n");
+    return list;
+}
 void print_list(struct guest *list){
     while (list != NULL) {
         printf("%-15s%-20s%-20s%5d\n", list->phone, list->last, list->first, list->party_size);
@@ -113,16 +153,16 @@ void clear_list(struct guest *list){
 }
 int read_line(char str[], int n)
 {
-  int ch, i = 0;
+    int ch, i = 0;
 
-  while (isspace(ch = getchar()))
-    ;
-  str[i++] = ch;
-  while ((ch = getchar()) != '\n') {
-    if (i < n)
-      str[i++] = ch;
-    
-   }
-   str[i] = '\0';
-   return i;
+    while (isspace(ch = getchar()))
+        ;
+    str[i++] = ch;
+    while ((ch = getchar()) != '\n') {
+        if (i < n)
+            str[i++] = ch;
+
+    }
+    str[i] = '\0';
+    return i;
 }
